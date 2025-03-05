cb9ff34ce246c39ce3ff72b2776635b2066afde2
abel2606 <abel.sanchez.guerrero@gmail.com>
2025-03-05 01:04:26 -0700

    Actividad finalizada

--- app/src/main/java/sanchez/abel/myfeelings/MainActivity.kt
+++ app/src/main/java/sanchez/abel/myfeelings/MainActivity.kt
@@ -1,12 +1,47 @@
 package sanchez.abel.myfeelings
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import sanchez.abel.myfeelings.utilities.CustomBarDrawable
+import sanchez.abel.myfeelings.utilities.CustomCircleDrawable
+import sanchez.abel.myfeelings.utilities.Emociones
+import sanchez.abel.myfeelings.utilities.JSONFile
 
 class MainActivity : AppCompatActivity() {
+    var jsonFile: JSONFile? = null
+    var veryHappy = 0.0F
+    var happy = 0.0F
+    var neutral = 0.0F
+    var sad = 0.0F
+    var verysad = 0.0F
+    var data: Boolean = false
+    var lista = ArrayList<Emociones>()
+    val graphVeryHappy: View = findViewById(R.id.graphVeryHappy)
+    val graphHappy: View = findViewById(R.id.graphHappy)
+    val graphNeutral: View = findViewById(R.id.graphNeutral)
+    val graphSad: View = findViewById(R.id.graphSad)
+    val graphVerySad: View = findViewById(R.id.graphVerySad)
+    val graph: View = findViewById(R.id.graph)
+    val guardarButton: Button = findViewById(R.id.guardarButton)
+    val veryHappyButton: ImageButton = findViewById(R.id.veryHappyButton)
+    val happyButton: ImageButton = findViewById(R.id.happyButton)
+    val neutralButton: ImageButton = findViewById(R.id.neutralButton)
+    val sadButton: ImageButton = findViewById(R.id.sadButton)
+    val verySadButton: ImageButton = findViewById(R.id.verySadButton)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +51,171 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        jsonFile = JSONFile()
+
+        fetchingData()
+        if(!data){
+            var emociones = ArrayList<Emociones>()
+            val fondo = CustomCircleDrawable(this, emociones)
+            graph.background = fondo
+            graphVeryHappy.background = CustomBarDrawable(this, Emociones("Muy feliz",0.0F, R.color.mustard, veryHappy))
+            graphHappy.background = CustomBarDrawable(this, Emociones("Feliz",0.0F, R.color.orange, happy))
+            graphNeutral.background = CustomBarDrawable(this, Emociones("Neutral",0.0F, R.color.greenie, neutral))
+            graphSad.background = CustomBarDrawable(this, Emociones("Triste",0.0F, R.color.blue, sad))
+            graphVerySad.background = CustomBarDrawable(this, Emociones("Muy triste",0.0F, R.color.deepblue, verysad))
+
+
+        }else{
+            actualizarGrafica()
+            iconoMayoria()
+        }
+
+        guardarButton.setOnClickListener {
+            guardar()
+        }
+
+        veryHappyButton.setOnClickListener{
+            veryHappy++
+            iconoMayoria()
+            actualizarGrafica()
+        }
+        happyButton.setOnClickListener{
+            happy++
+            iconoMayoria()
+            actualizarGrafica()
+        }
+        neutralButton.setOnClickListener{
+            neutral++
+            iconoMayoria()
+            actualizarGrafica()
+        }
+        sadButton.setOnClickListener{
+            sad++
+            iconoMayoria()
+            actualizarGrafica()
+        }
+        verySadButton.setOnClickListener{
+            verysad++
+            iconoMayoria()
+            actualizarGrafica()
+        }
+
+    }
+    fun fetchingData(){
+        try {
+            var json: String = jsonFile?.getData(this) ?: ""
+            if(json != ""){
+                this.data = true
+                var jsonArray: JSONArray = JSONArray(json)
+
+                this.lista = parseJson(jsonArray)
+
+                for(i in lista){
+                    when(i.nombre){
+                        "Muy feliz" -> veryHappy = i.total
+                        "Feliz" -> happy = i.total
+                        "Neutral" -> neutral = i.total
+                        "Triste" -> sad = i.total
+                        "Muy triste"-> verysad = i.total
+                    }
+                }
+            } else {
+                this.data = false
+            }
+        } catch (exception: JSONException){
+            exception.printStackTrace()
+        }
+    }
+
+    val icon: ImageView = findViewById(R.id.icon)
+    fun iconoMayoria(){
+        if(happy>veryHappy && happy>neutral && happy>sad && happy>verysad){
+            icon.setImageDrawable(resources.getDrawable(R.drawable.ic_happy))
+        }
+        if(veryHappy>happy && veryHappy>neutral && veryHappy>sad && veryHappy>verysad){
+            icon.setImageDrawable(resources.getDrawable(R.drawable.ic_veryhappy))
+        }
+        if(neutral>veryHappy && neutral>happy && neutral>sad && neutral>verysad){
+            icon.setImageDrawable(resources.getDrawable(R.drawable.ic_neutral))
+        }
+        if(sad>happy && sad>neutral && sad>veryHappy && sad>verysad){
+            icon.setImageDrawable(resources.getDrawable(R.drawable.ic_sad))
+        }
+        if(verysad>happy && verysad>neutral && verysad>sad && veryHappy<verysad){
+            icon.setImageDrawable(resources.getDrawable(R.drawable.id_verysad))
+        }
+    }
+
+    fun actualizarGrafica(){
+        val total = veryHappy+happy+neutral+verysad+sad
+
+        var pVH: Float = (veryHappy * 100 / total).toFloat()
+        var pH: Float = (happy * 100 /total).toFloat()
+        var pN: Float =(neutral*100 /total).toFloat()
+        var pS: Float = (sad*100 / total).toFloat()
+        var pVS: Float = (verysad * 100/ total).toFloat()
+
+        Log.d("porcentajes", "very happy "+ pVH)
+        Log.d("porcentajes", " happy "+ pH)
+        Log.d("porcentajes", "neutral "+ pN)
+        Log.d("porcentajes", "sad "+ pS)
+        Log.d("porcentajes", "very sad "+ pVS)
+
+        lista.clear()
+        lista.add(Emociones("Muy feliz", pVH, R.color.mustard, veryHappy))
+        lista.add(Emociones("Feliz", pH, R.color.orange, happy))
+        lista.add(Emociones("Neutral", pN, R.color.greenie, neutral))
+        lista.add(Emociones("Triste", pS, R.color.blue, sad))
+        lista.add(Emociones("Muy triste", pVS, R.color.deepblue, verysad))
+
+        val fondo = CustomCircleDrawable(this, lista)
+
+
+        graphVeryHappy.background = CustomBarDrawable(this, Emociones("Muy feliz", pVH, R.color.mustard, veryHappy))
+        graphHappy.background = CustomBarDrawable(this, Emociones("Feliz", pVH, R.color.orange, happy))
+        graphNeutral.background = CustomBarDrawable(this, Emociones("Neutral", pVH, R.color.greenie, neutral))
+        graphSad.background = CustomBarDrawable(this, Emociones("Triste", pVH, R.color.blue, sad))
+        graphVerySad.background = CustomBarDrawable(this, Emociones("Muy Triste", pVH, R.color.deepblue, verysad))
+
+        graph.background = fondo
+    }
+
+    fun parseJson (jsonArray: JSONArray): ArrayList<Emociones>{
+        var lista = ArrayList<Emociones>()
+
+        for(i in 0..jsonArray.length()){
+            try {
+                val nombre = jsonArray.getJSONObject(i).getString("nombre")
+                val porcentaje = jsonArray.getJSONObject(i).getDouble("porcentaje").toFloat()
+                val color = jsonArray.getJSONObject(i).getInt("color")
+                val total =jsonArray.getJSONObject(i).getDouble("total").toFloat()
+                var emocion = Emociones(nombre, porcentaje, color, total)
+                lista.add(emocion)
+            } catch (exception: JSONException){
+                exception.printStackTrace()
+            }
+        }
+        return lista
+    }
+
+    fun guardar(){
+        var jsonArray = JSONArray()
+        var o: Int =0
+        for(i in lista){
+            Log.d("objetos",i.toString())
+            var j: JSONObject = JSONObject()
+            j.put("nombre", i.nombre)
+            j.put("porcentaje", i.porcentaje)
+            j.put("color", i.color)
+            j.put("total", i.total)
+
+            jsonArray.put(o, j)
+            o++
+        }
+
+        jsonFile?.savaData(this, jsonArray.toString())
+
+        Toast.makeText(this, "Datos guardados", Toast.LENGTH_SHORT).show()
     }
 }
